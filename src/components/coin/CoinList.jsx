3b69edfdc19da233409ff3e5e79229db4963d0fe
MattKohn <kohney@hotmail.com>
2022-01-25 10:47:49 -0700

    Add DataGrid to CoinList component and pass through api data from coingecko.com to display the list of top 100 coins

--- src/components/coin/CoinList.jsx
+++ src/components/coin/CoinList.jsx
@@ -1,13 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
+import { DataGrid } from '@mui/x-data-grid';
 // import { Link } from 'react-router-dom';
+import columns from '../../functions/columns';
 
-const CoinList = () => {
+const CoinList = ({ id }) => {
+  const [coins, setCoins] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [selections, setSelections] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_rank&per_page500&page=1&sparkline=true')
+      .then((res) => res.json())
+      .then(data => {
+        setCoins(data);
+        setIsLoaded(true);
+      })
+  }, []);
+
+  // useEffect(() => {
+  //   fetch()
+  //     .then((res) => res.json())
+  //     .then(data => console.log(data))
+  // }, [selections])
+
+  
+  /*
+"https://assets.coingecko.com/coins/images/825/large/binance-coin-logo.png?1547034615"
+"https://assets.coingecko.com/coins/images/825/large/binance-coin-logo.png?1547034615"
+  */
+  const rows = coins.map((coin) => {
+    return {
+      id: coin.id,
+      rank: coin.market_cap_rank,
+      image: coin.image,
+      symbol: coin.symbol.toUpperCase(),
+      name: coin.name,
+      price: `$${coin.current_price.toFixed(2).toLocaleString()}`,
+      change: `${coin.price_change_percentage_24h.toFixed(2)}%`,
+    }
+  });
+
+  console.log(coins);
+  console.log(selections)
+
   return (
-    <div>
+    <div style={{ height: '75vh', width: '100%' }}>
       CoinList Component
-      {/* <li><Link to="/coins">Cryptocurrencies</Link></li> */}
+      {isLoaded ? 
+        <DataGrid
+          rows={rows}
+          columns={columns()}
+          pageSize={100}
+          rowsPerPageOptions={[100]}
+          checkboxSelection
+          disableSelectionOnClick
+          onSelectionModelChange={(checked) => {
+            setSelections(checked);
+          }}
+          selectionModel={selections}
+        />
+        :
+        <h2>Loading...</h2>
+      }
     </div>
-  );
-};
+  )
+}
 
-export default CoinList;
+export default CoinList
--- src/functions/columns.jsx
+++ src/functions/columns.jsx
@@ -1,3 +1,50 @@
 export default function columns() {
-  
+  return [
+    { field: 'rank', 
+      headerName: 'Rank', 
+      width: 80, 
+      sortable: true,
+      description: 'Rank by Market Cap Size',
+    },
+    {
+      field: 'image',
+      headerName: 'Logo',
+      renderCell: (params: GridRenderCellParams) => (
+        <>
+          {console.log(params)}
+          <img
+            src={params.formattedValue}
+            alt={params.id}
+            style={{width: 40}}
+            />
+        </>
+      ),
+      width: 100,
+      sortable: false,
+    },
+    {
+      field: 'symbol',
+      headerName: 'Symbol',
+      width: 140,
+    },
+    {
+      field: 'name',
+      headerName: 'Name',
+      width: 250,
+    },
+    {
+      field: 'price',
+      headerName: 'Current Value',
+      sortable: true,
+      type: 'number',
+      width: 140,
+    },
+    {
+      field: 'change',
+      headerName: '24hr Change',
+      sortable: true,
+      type: 'number',
+      width: 140,
+    },
+  ];
 }
