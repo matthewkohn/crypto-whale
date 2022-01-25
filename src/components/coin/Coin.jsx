37854fd2b030143da265773211a7fd1a606616a7
MattKohn <kohney@hotmail.com>
2022-01-25 15:07:26 -0700

    Add Sparklines and JSX to display coin data in Coin Component

--- src/components/coin/Coin.jsx
+++ src/components/coin/Coin.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 import formatCoinData from '../../functions/formatCoinData';
+
 
 const Coin = ({ coins }) => {
   const param = useParams();
   const coin = coins.filter(coin => coin.id === param.id)[0];
-  const [ imageUrl, id, name, symbol, price, rank, high, low, changePercent, marketCap, sparkline ] = formatCoinData(coin);
-
-  console.log(imageUrl, id, name, symbol, price, rank, high, low, changePercent, marketCap, sparkline )
+  const [ imageUrl, id, name, symbol, price, rank, high, low, changePercent, marketCap, sparklineData ] = formatCoinData(coin);
   
   return (
     <div>
-      Coin Component
-      {/* <img src={coin.image} alt={coin.id} />
-      <h2>{coin.name}</h2>
-      <p>{coin.symbol}</p>
-      <h3>${coin.current_price.toFixed(2)}</h3>
+      <img src={imageUrl} alt={id} />
+      <h2>{name}</h2>
+      <p>{symbol}</p>
+      <h3>{price}</h3>
       <ul>
-      {/* <li>{coin.}</li> */}
-      {/* </ul>  */}
+        <li>Market Cap: {marketCap}</li>
+        <li>Market Cap Rank: #{rank}</li>
+        <li>24-hour High: {high}</li>
+        <li>24-hour Low: {low}</li>
+        <li>24-hour Change: {changePercent}</li>
+      </ul>
+      
+      <Sparklines data={sparklineData} style={{background: "#00bdcc"}} margin={1} height={40} >
+        <SparklinesLine style={{ stroke: "white", fill: "none" }} />
+      </Sparklines>
     </div>
   );
 };
