408789d3dc8f2416de6935144f7047b4a97f87d8
MattKohn <kohney@hotmail.com>
2022-01-29 11:47:17 -0700

    Add useEffect to calculate quantity based on amount state

--- src/components/transaction/Transact.jsx
+++ src/components/transaction/Transact.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { FormControl, InputLabel, OutlinedInput, InputAdornment, Button } from '@mui/material';
+import { Box, Button, TextField } from '@mui/material';
 
 const Transact = ({ coin, cash, handleSetCash }) => {
   const [ownedCoin, setOwnedCoin] = useState({});
-  const [amountToTransact, setAmountToTransact] = useState('');
+  const [amount, setAmount] = useState('');
   const [tradeQty, setTradeQty] = useState(0);
+  // const [tradePrice, setTradePrice] = useState(0);
 
   useEffect(() => {
     fetch(`http://localhost:3001/coins?coinId=${coin.id}`)
@@ -14,61 +15,56 @@ const Transact = ({ coin, cash, handleSetCash }) => {
         console.log(`You don't own ${coin.id} yet`);
       })
     }, [coin.id]);
+
+  useEffect(() => {
+    const currentQty = (amount / coin.current_price).toFixed(6);
+    setTradeQty(currentQty)
+  }, [amount])
   
-  console.log(ownedCoin)
-  console.log(`Amount to transact: ${parseFloat(amountToTransact)}`)
-  console.log(tradeQty);
-  console.log(`Current price of ${coin.name}: ${coin.current_price}`)
-
-  const handleTradeQty = () => {
-    const qty = (parseFloat(amountToTransact) / coin.current_price).toFixed(6) * 10;
-    setTradeQty(qty);
-  }
 
 
 
-  const exampleData = {
-      coinId: "cardano",
-      quantity: 2000,
-      value: 100000
-  }
+  // const handlePost = () => {
+  //   fetch('http://localhost:3001/coins', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify(exampleData)
+  //   })
+  //     .then((res) => res.json())
+  //     .then(console.log)
+  // }
 
-  const handlePost = () => {
-    fetch('http://localhost:3001/coins', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(exampleData)
-    })
-      .then((res) => res.json())
-      .then(console.log)
-  }
-
-  const handleValueChange = (e) => {
-    setAmountToTransact(e.target.value);
-    handleTradeQty();
-  }
 
   return (
     <>
       <h2>Trade {coin.name}</h2>
       <h4>Cash available to trade: {cash}</h4>
-      <FormControl fullWidth sx={{ m: 1, width: '80%' }}>
-        <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
-        <OutlinedInput
-          id="outlined-adornment-amount"
-          value={amountToTransact}
-          onChange={handleValueChange}
-          startAdornment={<InputAdornment position="start">$</InputAdornment>}
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+         <TextField
+          id="outlined-number"
           label="Amount"
-          size="medium"
+          type="number"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          InputLabelProps={{
+            shrink: true,
+          }}
         />
         <h4>Quantity: {tradeQty}</h4>
-
-        <Button onClick={() => handlePost()} variant="contained">Buy</Button>
+        {/* <Button onClick={() => handlePost()} variant="contained">Buy</Button> */}
         <Button variant="contained">Sell</Button>
-      </FormControl>
+      </Box>
+        
+
       <h2>Your Portfolio</h2>
       <h4>Number of { coin.name } Owned: { ownedCoin ? ownedCoin.quantity : 0 }</h4>
       <h4>Current Value: ${ ownedCoin ? ownedCoin.value : 0 }</h4>
